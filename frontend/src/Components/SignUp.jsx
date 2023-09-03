68a9827e3fed0ed06be9e2e51a18b59f006bc48d
Harun Mansuri <121337475+MansuriHarun@users.noreply.github.com>
2023-09-03 11:54:02 +0530

    Update SignUp.jsx

--- frontend/src/Components/SignUp.jsx
+++ frontend/src/Components/SignUp.jsx
@@ -24,6 +24,8 @@ const SignUp = () => {
             headers: {
                 "Content-Type": "application/json",
             },
+            mode: "cors",
+            credentials: "include",
             body: JSON.stringify({
                 email,
                 password
