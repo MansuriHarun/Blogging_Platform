4e8a77f1219ffe1e8a73bad4c2d5fa463233c472
Harun Mansuri <121337475+MansuriHarun@users.noreply.github.com>
2023-09-03 18:55:05 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(
     cors({
         origin: [
-            "https://harun-blog-website.netlify.app",
+            "*",
         ],
         credentials: true,
     }));
