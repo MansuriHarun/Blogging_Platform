2858f044efd9ce2d626f0edd61aa09600ae423d0
Harun Mansuri <121337475+MansuriHarun@users.noreply.github.com>
2023-09-03 18:57:42 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,13 @@ connectDatabase();
 
 
 // Middleware
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*'); 
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Accept');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+})
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
