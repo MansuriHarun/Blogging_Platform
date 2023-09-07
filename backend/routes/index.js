efec344ba1b0cf0011699f313cd1c5166f273c14
Harun Mansuri <121337475+MansuriHarun@users.noreply.github.com>
2023-09-07 14:29:45 +0530

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -9,30 +9,37 @@ router.get("/blog/:id", async (req, res) => {
     const {id} = req.params;
     const getBlog = await Blog.findById(id);
     res.send(getBlog);
-}).delete("/delete/:id", async(req, res) => {
-    const { id } = req.params;
+}).delete("/delete/:id", async (req, res) => {
     try {
+        const { id } = req.params;
+        const userId = sessionStorage.getItem("UserId");
+
+        // Check if the user is logged in
+        if (!userId) {
+            return res.status(401).json({ message: "You must be logged in" });
+        }
+
         const getBlog = await Blog.findById(id);
 
+        // Check if the blog exists
         if (!getBlog) {
             return res.status(404).json({ message: "Blog not found" });
         }
 
-        const UserId = sessionStorage.getItem("UserId");
-        if (UserId) {
-            // Check if the user's ID matches the author's ID of the blog
-            if (getBlog.author.toString() === userId) {
-                await Blog.deleteOne({ _id: id });
-                return res.status(200).json({ message: "Deleted" });
-            } else {
-                return res.status(403).json({ message: "Unauthorized to delete this blog" });
-            }
-        } else {
-            return res.status(401).json({ message: "You must be logged in" });
+        // Check if the user's ID matches the author's ID of the blog
+        if (getBlog.author.toString() !== userId) {
+            return res.status(403).json({ message: "Unauthorized to delete this blog" });
         }
+
+        // Delete the blog
+        await Blog.deleteOne({ _id: id });
+
+        return res.status(200).json({ message: "Deleted" });
     } catch (error) {
-        res.status(500).json({ error: "Failed to delete blog" });
+        console.error(error);
+        return res.status(500).json({ error: "Failed to delete blog" });
     }
 });
+
 
 module.exports = router;
