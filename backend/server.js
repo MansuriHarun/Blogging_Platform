51bcadeb37f3c5029b6406214d3845d176ca5b3b
Harun Mansuri <121337475+MansuriHarun@users.noreply.github.com>
2023-09-07 13:38:20 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ connectDatabase();
 
 // Middleware
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://harun-blog-website.netlify.app');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Accept');
   res.setHeader('Access-Control-Allow-Credentials', true);
