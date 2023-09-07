46292900f09db5e4b2ab8e7b43b3ff8b3a514dfa
Harun Mansuri <121337475+MansuriHarun@users.noreply.github.com>
2023-09-07 12:38:04 +0530

    Update AllBlogs.jsx

--- frontend/src/Components/AllBlogs.jsx
+++ frontend/src/Components/AllBlogs.jsx
@@ -3,12 +3,9 @@ import axios from "axios";
 import "../index.css";
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Cookies from "js-cookie";
-import jwt_decode from "jwt-decode"
 
 const AllBlogs = () => {
     const [ blogs, setBlogs ] = useState([]);
-    const [decodedToken, setDecodedToken] = useState(null);
     useEffect(() => {
         fetch("https://blogging-platform-api.onrender.com/allblogs", {
             mode: "cors",
@@ -16,15 +13,7 @@ const AllBlogs = () => {
             .then((res) => res.json())
             .then((data) => setBlogs(data))
             .catch((err) => console.log(err))
-            const loggedInUserId = Cookies.get("UserId");
-            if (loggedInUserId) {
-                try {
-                    const decoded = jwt_decode(loggedInUserId);
-                    setDecodedToken(decoded._id);
-                } catch (error) {
-                    console.error('Error decoding token:', error.message);
-                }
-            }
+            const loggedInUserId = localStorage.getItem("UserId");
     }, []);
     const handleDelete = (id) => {
         axios.delete("https://blogging-platform-api.onrender.com/delete/" + id)
@@ -43,7 +32,7 @@ const AllBlogs = () => {
                         <h2 className="blog-item-title">{blog.title}</h2>
                         <p className="blog-item-description">{blog.description}</p>
                         <p className="blog-item-date">{blog.createdAt}</p>
-                        {decodedToken === blog.author && (
+                        {loggedInUserId === blog.author && (
                                 <button className="delete-button" onClick={() => handleDelete(blog._id)}>Delete</button>
                             )}
                         </li>
